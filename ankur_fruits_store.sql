5b27769ee68d65e5d0e735b61ad952dd042a0570
Ankur Shukla <shukla.ankur854@gmail.com>
2026-04-17 16:25:46 +0530

    ankur_fruits_store.sql

--- ankur_fruits_store.sql
+++ ankur_fruits_store.sql
@@ -557,11 +557,13 @@ FROM jan_sales s
 INNER JOIN customers c
 ON s.customer_id = c.customer_id;
 '''
+'''sql
 SELECT p.product_name, SUM(s.quantity*p.price) AS revenue
 FROM jan_sales s
 INNER JOIN products p
 ON s.product_id = p.product_id
 GROUP BY p.product_name;
+'''
 '''sql
 SELECT c.customer_name, SUM(s.quantity) AS total_purchase
 FROM jan_sales s
